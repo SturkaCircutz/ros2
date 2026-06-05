b032405a0f268d09635830e3205c986fdc240e8f
jiawen <313721325sjw@gmail.com>
2026-06-05 17:52:45 +0800

    subscriber added

--- tutorial_pkg/src/my_first_node.cpp
+++ tutorial_pkg/src/my_first_node.cpp
@@ -1,12 +1,37 @@
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+// in order to process msg from the camera
+using namespace std::placeholders;
+
 class MyNode : public rclcpp::Node
 {
     public:
         MyNode(): Node("my_node"){ // mynode is a public subclass of rclcpp node
             // rclcpp info are preferred over printf
+
+            //create substription is a template function
+            auto qos = rclcpp::QoS(10).reliable();
+            subscriber_ = create_subscription<sensor_msgs::msg::Image>(
+                "/image", qos, std::bind(&MyNode::image_callback, this, _1));
             RCLCPP_INFO(get_logger(), "node started!");
         }
-        
+    private:
+        void image_callback(const sensor_msgs::msg::Image::SharedPtr image){
+            if (image->data.empty()) {
+                RCLCPP_WARN(get_logger(), "received empty image");
+                return;
+            }
+
+            long long sum = 0;
+            for(uint8_t value : image->data){
+                sum+=value;
+            }
+            double avg = static_cast<double>(sum) / image->data.size();
+            RCLCPP_INFO(get_logger(), "brightness average: %.2f", avg);
+        }
+
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
 };
 
 int main(int argc, char ** argv){
@@ -17,3 +42,7 @@ int main(int argc, char ** argv){
     return 0;
 
 }
+
+
+// subscriber is used to respond the speaker by invoking some code instead of printing 
+//the things that are sent by speaker.
