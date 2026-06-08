c0bb428f9da68a11402e49b151b89969d72b912c
jiawen <313721325sjw@gmail.com>
2026-06-08 11:55:19 +0800

    clear

--- build_humble/tutorial_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ws/tutorial_pkg
-# Build directory: /ws/build_humble/tutorial_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tutorial_pkg/src/my_first_node.cpp
+++ tutorial_pkg/src/my_first_node.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "std_msgs/msg/u_int8.hpp"
@@ -7,6 +8,7 @@
 
 // in order to process msg from the camera
 using namespace std::placeholders;
+using namespace std::chrono_literals;
 
 class MyNode : public rclcpp::Node
 {
@@ -15,14 +17,22 @@ class MyNode : public rclcpp::Node
             // rclcpp info are preferred over printf
 
             //create substription is a template function
+            declare_parameter("timer_period_s", 5); // if timer_period_s does not set use 5
+            auto timer_period_s = std::chrono::seconds(get_parameter("timer_period_s").as_int());
             auto qos = rclcpp::QoS(10).reliable();
             subscriber_ = create_subscription<sensor_msgs::msg::Image>(
                 "/image", qos, std::bind(&MyNode::image_callback, this, _1));
             
-                publisher_ = create_publisher<std_msgs::msg::UInt8>("/brightness", rclcpp::SensorDataQos());
+            publisher_ = create_publisher<std_msgs::msg::UInt8>("/brightness", 10);
             RCLCPP_INFO(get_logger(), "node started!");
+            timer_ = create_wall_timer(timer_period_s, std::bind(&MyNode::timer_callback, this));
+            // call the timer callback function after counting down 5 seconds.
         }
     private:
+        void timer_callback(){
+            RCLCPP_INFO(get_logger(), "Timer activate");
+            // print info to notify that timer is activated.
+        }
         void image_callback(const sensor_msgs::msg::Image::SharedPtr image){
             if (image->data.empty()) {
                 RCLCPP_WARN(get_logger(), "received empty image");
@@ -38,13 +48,16 @@ class MyNode : public rclcpp::Node
             
 
             std_msgs::msg::UInt8 brightness_msg;
-            brightness_msg.data = avg;
+            brightness_msg.data = static_cast<uint8_t>(
+                std::clamp(std::lround(avg), 0L, 255L));
             publisher_->publish(brightness_msg);
 
             RCLCPP_INFO(get_logger(), "brightness average: %.2f", avg);
         }
 
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+        rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
         
 };
 
